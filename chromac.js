bbd9a5eda06dd694e76694771b10b7d4a182a267
Filip Tepper <filip@tepper.pl>
2010-05-19 20:52:57 +0200

    added support for Control key

--- chromac.js
+++ chromac.js
@@ -1,7 +1,7 @@
 var Chromac = (function(){
   return {
     handleBookmarksBar: function(e) {
-      if (e.metaKey === true && (e.keyCode >= 49 && e.keyCode <= 57)) {
+      if ((e.metaKey === true || e.ctrlKey === true) && (e.keyCode >= 49 && e.keyCode <= 57)) {
         chrome.extension.sendRequest({ name: "bookmarksBar", id: (-48 + parseInt(e.keyCode)) });
         e.preventDefault();
       }
