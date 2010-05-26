fc8fa100f75fdbec27f487bf32b81486d2d8230b
Filip Tepper <filip@tepper.pl>
2010-05-26 20:31:42 +0200

    version bump, option/alt opens bookmark in new window

--- chromac.js
+++ chromac.js
@@ -2,7 +2,7 @@ var Chromac = (function(){
   return {
     handleBookmarksBar: function(e) {
       if ((e.metaKey === true || e.ctrlKey === true) && (e.keyCode >= 49 && e.keyCode <= 57)) {
-        chrome.extension.sendRequest({ name: "bookmarksBar", id: (-48 + parseInt(e.keyCode)) });
+        chrome.extension.sendRequest({ name: "bookmarksBar", id: (-48 + parseInt(e.keyCode)), new_tab: e.altKey });
         e.preventDefault();
       }
     }
